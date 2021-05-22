49d346f6c2f7907feee770e3eb40cc84f32baf33
rubens-lavor <haryson07@gmail.com>
2021-05-22 17:02:03 -0300

    continuação do exercício

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -13,4 +13,15 @@ export class ShoppingCart {
   removeItem(index: number): void {
     this._items.splice(index, 1)
   }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items
+  }
 }
+
+const shoppingCart = new ShoppingCart()
+shoppingCart.addItem({ name: 'Camiseta', price: 49.9 })
+shoppingCart.addItem({ name: 'Caderno', price: 9.9 })
+shoppingCart.addItem({ name: 'Lapis', price: 1.59 })
+
+console.log(shoppingCart.items)
