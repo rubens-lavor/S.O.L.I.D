fb36706b8e538c53739a6a413adb08c42bec4924
rubens-lavor <haryson07@gmail.com>
2021-05-24 23:38:43 -0300

    aula 1 srp finalizada

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -3,8 +3,11 @@ type CartItem = {
   price: number
 }
 
+type OrderStatus = 'open' | 'closed'
+
 export class ShoppingCart {
   private readonly _items: CartItem[] = []
+  private _orderStatus: OrderStatus = 'open'
 
   addItem(item: CartItem): void {
     this._items.push(item)
@@ -17,11 +20,52 @@ export class ShoppingCart {
   get items(): Readonly<CartItem[]> {
     return this._items
   }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus
+  }
+
+  total(): number {
+    return this._items.reduce((total, next) => total + next.price, 0)
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('seu carrinho está vazio')
+      return
+    }
+
+    this._orderStatus = 'closed'
+    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido`)
+    this.saveOrder()
+    this.clear()
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi esvaziado')
+    this._items.length = 0
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso')
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada: ', msg)
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0
+  }
 }
 
 const shoppingCart = new ShoppingCart()
-shoppingCart.addItem({ name: 'Camiseta', price: 49.9 })
-shoppingCart.addItem({ name: 'Caderno', price: 9.9 })
+shoppingCart.addItem({ name: 'Camiseta', price: 49.91 })
+shoppingCart.addItem({ name: 'Caderno', price: 9.91 })
 shoppingCart.addItem({ name: 'Lapis', price: 1.59 })
 
 console.log(shoppingCart.items)
+console.log(shoppingCart.total())
+console.log(shoppingCart.orderStatus)
+shoppingCart.checkout()
+console.log(shoppingCart.orderStatus)
