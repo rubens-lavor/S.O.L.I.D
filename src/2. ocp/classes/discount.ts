0d8b69ea2e5e5a40c5ca20d2008d91f5c8a2f551
rubens-lavor <haryson07@gmail.com>
2021-05-27 20:44:01 -0300

    refatoração classes de desconto

--- src/2. ocp/classes/discount.ts
+++ src/2. ocp/classes/discount.ts
@@ -1,23 +1,17 @@
 export abstract class Discount {
-  abstract calculate(value: number): number
+  protected discount = 0
+
+  calculate(price: number): number {
+    return price * (1 - this.discount)
+  }
 }
 
 export class FiftyPercentDiscount extends Discount {
-  private readonly discount: number = 0.5
-  calculate(price: number): number {
-    return price * (1 - this.discount)
-  }
+  protected readonly discount: number = 0.5
 }
 
 export class TenPercentDiscount extends Discount {
-  private readonly discount: number = 0.1
-  calculate(price: number): number {
-    return price * (1 - this.discount)
-  }
+  protected readonly discount: number = 0.1
 }
 
-export class NoDiscount extends Discount {
-  calculate(price: number): number {
-    return price
-  }
-}
+export class NoDiscount extends Discount {}
